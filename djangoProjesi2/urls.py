66c73a5fb9ef70b337d5a6c5066910566356e224
Emre Kirik <mertkirik46@gmail.com>
2020-04-24 05:12:23 +0300

    DjangoProjesi2 canliya alma islemi

---
+++ djangoProjesi2/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from blogSitesi import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.gonderi_listesi, name='gonderi_listesi'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
